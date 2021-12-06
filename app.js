09dae16878216a221898037cc64716797f48d8dc
Katrina Apel <kapel2810@gmail.com>
2021-12-06 07:17:22 +0200

    ex2 done

--- app.js
+++ app.js
@@ -2,31 +2,66 @@
 ///////////////////////////////////
 // ----------ex1------------
 
-const generate7BoomAfterDelayAsync = (min, max) => {
+// const generate7BoomAfterDelayAsync = (min, max) => {
+//     let myPromise = new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             let randomNumber = Math.floor(Math.random() * (max - min)) + min
+//             // console.log(randomNumber)
+
+//             if (randomNumber % 7 === 0 || randomNumber % 10 === 7) {
+//                 resolve(randomNumber)
+//             } else {
+//                 reject("err")
+//             }
+//         }, 1000);
+//     })
+//     return myPromise
+
+
+// }
+
+
+// const runCode = () => {
+//     generate7BoomAfterDelayAsync(1, 100)
+//     .then((data)=>{
+//         console.log(data)
+//     })
+//     .catch((err)=> {
+//         console.log(err)
+//     })
+// }
+
+/////////////////////////////////////
+//----------- ex2--------------
+
+
+const generatePrimeNumberAfterDelayAsync = (min, max) => {
     let myPromise = new Promise((resolve, reject) => {
         setTimeout(() => {
             let randomNumber = Math.floor(Math.random() * (max - min)) + min
-            // console.log(randomNumber)
 
-            if (randomNumber % 7 === 0 || randomNumber % 10 === 7) {
+            if (randomNumber < 2) {
+                reject()
+
+                for (let i = 0; i < randomNumber; i++) {
+                    if (randomNumber % i === 0) {
+                        reject()
+                    }
+                }
+            }else{
                 resolve(randomNumber)
-            } else {
-                reject("err")
             }
         }, 1000);
     })
     return myPromise
-
-
 }
 
-
 const runCode = () => {
-    generate7BoomAfterDelayAsync(1, 100)
-    .then((data)=>{
-        console.log(data)
-    })
-    .catch((err)=> {
-        console.log(err)
-    })
+    generatePrimeNumberAfterDelayAsync(1, 20)
+        .then((number) => {
+            console.log(number)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
 }
